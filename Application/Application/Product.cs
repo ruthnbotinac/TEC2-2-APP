d26986337c00efaa0f07aa14ffeba7cabf636a44
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-08-29 21:25:58 -0500

    Creating Category

--- Application/Application/Product.cs
+++ Application/Application/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,26 @@ namespace Application
 {
     internal class Product
     {
+        public string[] category = { "Lacteos", "Semillas", "Cultivos", "Trabajo" , "Frutas", "Vegetales", "Animales", "Otro" };
+        public void productCategory(string prod)
+        {
+
+            for(int i=0;i<8;i++){
+                if (category[i] == prod) { 
+                
+                }
+            }
+          //  get { return $"{} {}"; }
+        }
+
+        //Lacteos
+        //Semillas
+        //Cultivos
+        //Trabajo
+        //Frutas
+        //Vegetales
+        //Animales
+        //Otros
 
     }
 }
