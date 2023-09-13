e6b15b56e9c7685029e8dff6f14ffe2e96cc72c0
OscarDanielArcos <125132408+OscarDanielArcos@users.noreply.github.com>
2023-09-12 21:16:32 -0500

    ENCAPSULAMIENTO
    Se realiza encapsulamiento y se añaden propiedades

--- Application/Application/FigurasPlanas.cs
+++ Application/Application/FigurasPlanas.cs
@@ -3,10 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Application
 {
-    internal class FigurasPlanas
+    public class FigurasPlanas
     {
+        private double _lado { get; set; }
+        public double Lado
+        {
+            get { return _lado; }
+            set { _lado = value; }
+        }
+
+        private double _base { get; set; }
+        public double Base
+        {
+            get { return _base; }
+            set { _base = value; }
+        }
+
+        private double _altura { get; set; }
+        public double Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        public static void ExecuteExercise()
+        {
+            Console.WriteLine("Elegir una de las opciones");
+            string opc;
+            opc = Console.ReadLine();
+            Console.WriteLine("La opcion es " + opc);
+            switch (opc)
+            {
+                case "1":
+                    AreaCuadrado();
+                    break;
+
+                case "2":
+                    AreaRectangulo();
+                    break;
+
+                case "3":
+                    AreaTriangulo();
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+
+        public static void AreaCuadrado()
+        {
+            FigurasPlanas figuras = new FigurasPlanas();
+
+            Console.WriteLine();
+            Console.WriteLine("Área de Cuadrado");
+            Console.Write("Ingresa un lado del cuadrado: ");
+
+            figuras.Lado = double.Parse(Console.ReadLine());
+
+            double area = figuras.Lado * figuras.Lado;
+            Console.WriteLine("");
+            Console.WriteLine($"El área del cuadrado con lado {figuras.Lado} es: {area}");
+        }
+
+        public static void AreaRectangulo()
+        {
+            FigurasPlanas figuras = new FigurasPlanas();
+
+            Console.WriteLine("Area del Rectangulo");
+            Console.Write("Ingresa la base del Rectangulo: ");
+            figuras.Base = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa la altura del Rectangulo: ");
+            figuras.Altura = double.Parse(Console.ReadLine());
+
+            double Area = figuras.Base * figuras.Altura;
+
+            Console.WriteLine("");
+            Console.WriteLine($"El Area del Rectangulo es: {Area}");
+        }
+
+        public static void AreaTriangulo()
+        {
+            FigurasPlanas figuras = new FigurasPlanas();
+
+            Console.WriteLine("Area del Triangulo");
+            Console.Write("Ingresa la base del Triangulo: ");
+            figuras.Base = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa la altura del Triangulo: ");
+            figuras.Altura = double.Parse(Console.ReadLine());
+
+            double Area = (figuras.Base * figuras.Altura) / 2;
+
+            Console.WriteLine("");
+            Console.WriteLine($"El Area del Triangulo es: {Area}");
+        }
     }
+
 }
--- Application/Application/Program.cs
+++ Application/Application/Program.cs
@@ -4,9 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Elija la forma geométrica");
-            Console.WriteLine("Eliga: area, perítimetro, volumen ");
 
+            Console.WriteLine("Bienvenido");
+            Console.WriteLine("Elija la opcion que desea");
+            Console.WriteLine("(1) Figuras Plana");
+            Console.WriteLine("(2)");
+            Console.WriteLine("_______________________________\n");
+
+            string opc;
+            opc = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("La opcion elegida es" + ":" + opc);
+
+            Console.WriteLine("_______________________________\n");
+            Console.WriteLine("Figuras Planas\n");
+            Console.WriteLine("");
+            Console.WriteLine("Eliga una de las opciones de las cuales quiere hallar el Area\n");
+
+            Console.WriteLine("1" + " : " + "Area Cuadrado");
+            Console.WriteLine("2" + " : " + "Area Rectangulo");
+            Console.WriteLine("3" + " : " + "Area Triangulo\n");
+
+            Console.WriteLine("_______________________________\n");
+
+            switch (opc)
+            {
+                case "1":
+                    FigurasPlanas.ExecuteExercise();
+                    break;
+
+
+            }
         }
+
     }
 }
