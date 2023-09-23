bdd7ff054e81caa8cf8184dc70a8b4fdda286a45
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-23 10:17:15 -0500

    Encapsulamiento

--- Application/Application/Lacteos.cs
+++ Application/Application/Lacteos.cs
@@ -9,5 +9,12 @@ namespace Application
     internal class Lacteos:Product
     {
         private DateTime fechaDeVencimiento;
+        private DateTime createdDay;
+        private DateTime createdMonth;
+        private DateTime createdYear;
+        private DateTime createdTime;
+        private DateTime updatedTime;
+
+
     }
 }
--- Application/Application/Location.cs
+++ Application/Application/Location.cs
@@ -8,6 +8,44 @@ namespace Application
 {
     internal class Location
     {
+        private long latitud;
+        private long longitud;
+        private string city;
+        private string country;
+        private string region;
+        private string updatelocation;
+
+        public long Latidud
+        {
+            get { return latitud; }
+            set {   latitud = value; }
+        }
+        public long Longitud
+        {
+            get { return longitud; }
+            set { longitud = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        
+        }
+        public string Country
+        {
+            get { return country; } 
+            set { country = value; }
+        }
+        public string Region
+        {
+            get { return region; }
+            set { region = value; }
+        }
+        public string Updatelocation
+        {
+            get { return updatelocation; }
+            set { updatelocation = value; }
+        }
 
     }
 }
